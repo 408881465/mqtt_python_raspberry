2de15895eb70857bc92da65b83a47414ea5443ae
Leonel Espinoza <leonelespinoza15@gmail.com>
2017-02-17 23:55:12 +0000

    raspberry_3_mqttpaho

---
+++ cloud.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO 
+import time 
+import paho.mqtt.client as mqtt 
+GPIO.setmode(GPIO.BOARD) 
+GPIO.setup(13,GPIO.IN) 
+mqttc=mqtt.Client() 
+mqttc.username_pw_set(username="lionell", password="150390")
+mqttc.connect("m10.cloudmqtt.com",11793,60) 
+mqttc.loop_start() 
+def reading1():
+    a=GPIO.input(13)
+    print(a)
+    return a 
+while 1:
+    t=reading1()
+    (result,mid)=mqttc.publish("temp",t,2)
+    time.sleep(1) 
+mqttc.loop_stop()
+mqttc.disconnect()
